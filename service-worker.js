f5b2dec122fcc9b7dc5393d6054a49e2a99e6e34
Martin Brändli <ma.braendli@gmail.com>
2024-11-14 16:14:35 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,15 @@
-const CACHE_NAME = 'agenda-cache-v1';
-const urlsToCache = [
-  '/',
-  '/read.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => 
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open("agenda-cache").then(cache => {
+            return cache.addAll(["/", "/index.html", "/styles.css", "/app.js"]);
         })
-      )
-    )
-  );
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
